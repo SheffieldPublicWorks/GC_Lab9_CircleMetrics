cbead84483c81582953ba4601da4f21cb639d2d5
unknown <stephen.scobie@gmail.com>
2017-11-03 02:08:53 -0400

    Initial commit.

---
+++ GC_Deliverable10_Lab9_CircleMetrics/Circle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace GC_Deliverable10_Lab9_CircleMetrics
+{
+    class Circle
+    {
+        //data
+        private double radius;
+        public static int circCount = 0;
+
+        //constructors
+        public Circle (double radius)
+        {
+            this.radius = radius;
+            circCount++;
+        }
+
+        //getters
+        public double GetRadius ()
+        {
+            return radius;
+        }
+
+        //setters
+        public void SetRadius (double radius)
+        {
+            this.radius = radius;
+        }
+
+        //methods
+        private string FormatNumber (double x)
+        {
+            x = (long)(x * 100 + 0.5) / 100.0;
+            string str = x.ToString();
+            return str;
+        }
+
+        public double GetArea ()
+        {
+            return Math.PI * (radius * radius);
+        }
+
+        public double GetCircumference ()
+        {
+            return Math.PI * radius * 2;
+        }
+
+        public string GetFormattedArea ()
+        {
+            return FormatNumber(GetArea());
+        }
+
+        public string GetFormattedCircumference ()
+        {
+            return FormatNumber(GetCircumference());
+        }
+
+        //properties (extended exercise)
+    }
+}
---
+++ GC_Deliverable10_Lab9_CircleMetrics/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_Deliverable10_Lab9_CircleMetrics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/************************************/");
+            Console.WriteLine("/*       Circles of Fury v1.0       */");
+            Console.WriteLine("/************************************/");
+            Console.WriteLine();
+
+            double radius;
+            bool continueProcessing = true;
+            while (continueProcessing)
+            {
+                bool valid = false;
+                while (!valid)
+                {
+                    Console.Write("Please enter the radius of a circle to calculate it's area and circumference: ");
+
+                    valid = Double.TryParse(Console.ReadLine(), out radius);
+                    if (!valid || radius <= 0)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("ERROR: Please enter a valid number for the radius.");
+                    }
+                    else
+                    {
+                        Circle userCircle = new Circle(radius);
+                        Console.WriteLine($"The circumference is: {userCircle.GetFormattedCircumference()}");
+                        Console.WriteLine($"The area is: {userCircle.GetFormattedArea()}");
+                        Console.WriteLine();
+                        valid = true;
+                    }
+                }
+
+                Console.Write("Do you want to continue entering radii for circles? (y/n): ");
+                if (Console.ReadLine().ToLower() == "y")
+                {
+                    continueProcessing = true;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Thanks for using Circles of Fury. You created {0} circles", Circle.circCount);
+                    Console.WriteLine("Hope to see you soon.");
+                    continueProcessing = false;
+                }
+            }
+        }
+    }
+}
